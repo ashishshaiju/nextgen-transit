559fd583cab06f918482480acd6dde40df98f367
Albin Varghese <albin8285@gmail.com>
2024-03-16 15:22:55 +0530

    chore: add semester table parameters

--- web/database/migrations/2024_03_16_094658_create_semesters_table.php
+++ web/database/migrations/2024_03_16_094658_create_semesters_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('semesters', function (Blueprint $table) {
             $table->id();
+            $table->string("code");
+            $table->string("name");
+            $table->integer("year");
             $table->timestamps();
         });
     }
