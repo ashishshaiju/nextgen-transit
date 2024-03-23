490475e7fd90af835bf95027dcabc75bd6b47252
Albin Varghese <albin8285@gmail.com>
2024-03-23 10:48:51 +0530

    chore: update attributes for staff table

--- web/database/migrations/2024_03_23_050852_create_staff_table.php
+++ web/database/migrations/2024_03_23_050852_create_staff_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('staff', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('bus_id')->constrained();
+            $table->string('department');
+            $table->string('designation');
             $table->timestamps();
         });
     }
