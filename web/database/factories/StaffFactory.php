317427faf35a0fb2d35a9e81c6d7a025396df9bb
Albin Varghese <albin8285@gmail.com>
2024-03-23 10:54:01 +0530

    chore: factory for staff

--- web/database/factories/StaffFactory.php
+++ web/database/factories/StaffFactory.php
@@ -17,7 +17,12 @@ class StaffFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'department' => $this->faker->randomElement(['CSE', 'CE', 'ECE', 'EEE', 'ME', 'MBA']),
+            'designation' => $this->faker->randomElement(['Teacher', 'Accountant', 'Librarian', 'Lab Assistant']),
+            // override the bus_id to be a random bus id if not provided
+            'bus_id' => \App\Models\Bus::factory(),
+            // override the user_id to be a random user id if not provided
+            'user_id' => \App\Models\User::factory(),
         ];
     }
 }
