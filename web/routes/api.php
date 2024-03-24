4f694c76d4e92f0b2e41596656e9b57f6277cfc4
Albin Varghese <albin8285@gmail.com>
2024-03-24 20:17:15 +0530

    feat: add api ping route

--- web/routes/api.php
+++ web/routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// ping route
+Route::get('/ping', function () {
+    return response()->json(['message' => 'pong'], 200);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
