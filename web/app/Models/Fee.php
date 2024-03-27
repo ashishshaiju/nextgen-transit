8a8333fd74a35f1f6d3d74d799f9f49f88fa1e29
Albin Varghese <albin8285@gmail.com>
2024-03-27 11:25:33 +0530

    chore: add parameters to fee model

--- web/app/Models/Fee.php
+++ web/app/Models/Fee.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Fee extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'student_semester_id',
+        'due_amount',
+        'due_date',
+    ];
+
+    public function studentSemester(): BelongsTo
+    {
+        return $this->belongsTo(StudentSemester::class);
+    }
 }
--- web/database/migrations/2024_03_27_054158_create_fees_table.php
+++ web/database/migrations/2024_03_27_054158_create_fees_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('fees', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('student_semester_id')->constrained()->onDelete('cascade');
+            $table->decimal('due_amount', 10, 2);
+            $table->date('due_date');
             $table->timestamps();
         });
     }
