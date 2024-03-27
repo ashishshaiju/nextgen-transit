1c799ebc5c607acd840654a668a63a2fe8faaa9a
Albin Varghese <albin8285@gmail.com>
2024-03-27 11:50:35 +0530

    chore: update user access logs relation

--- web/app/Models/User.php
+++ web/app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -97,4 +98,10 @@ class User extends Authenticatable
     {
         return $this->hasOne(Admin::class);
     }
+
+    public function accessLogs(): HasMany
+    {
+        return $this->hasMany(AccessLog::class);
+    }
+
 }
